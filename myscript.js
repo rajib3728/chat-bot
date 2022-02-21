2b2f3727423bedeeca003ced16d5ab97ea4464d4
Rajib Paul <53445533+rajib3728@users.noreply.github.com>
2022-02-21 14:19:29 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,12 @@
+
+function fun4()
+{
+    window.open("https://sites.google.com/view/the-earth-infomation/home");
+}
+function fun1()
+{
+    var x=document.getElementById("fname").value;
+    var y="Welcome"+" "+x;
+   var m= document.getElementsByClassName("st1");
+   m[0].innerHTML=y;
+}
